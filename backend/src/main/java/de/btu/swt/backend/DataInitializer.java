8ddf7530ba0ba7f244a0bed9d456eaa8de5e85c1
Mathias Schubanz <M.Schubanz@b-tu.de>
2021-05-16 14:28:28 +0200

    updated DataInitializer

--- backend/src/main/java/de/btu/swt/backend/DataInitializer.java
+++ backend/src/main/java/de/btu/swt/backend/DataInitializer.java
@@ -116,10 +116,10 @@ public class DataInitializer {
         // create language server objects for which language server instances will be built and started later
 
         LanguageServer metaServer = LanguageServer.builder()
-                .languageName("simple-decision-record-language")
+                .languageName("simple_decision_record_language")
                 .build();
         LanguageServer mdrSimpleServer = LanguageServer.builder()
-                .languageName("simple-decision-record-language")
+                .languageName("simple_decision_record_language")
                 .build();
 
 
