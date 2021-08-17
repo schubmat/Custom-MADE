2309ee8c8804ec99235ccc2659db5f045329c2ee
Robert Richter <robert.richter@b-tu.de>
2021-08-17 09:37:42 +0200

    Removed .browsermodules

--- dedoc-editor/mdr-dsl/lib/common/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MDR_LANGUAGE_FILE_EXTENSION = 'mydsl';
-exports.MDR_LANGUAGE_SERVER_ID = 'MyDsl';
-exports.MDR_LANGUAGE_SERVER_NAME = 'MyDsl';
-//# sourceMappingURL=index.js.map
--- dedoc-editor/mdr-dsl/lib/node/mdr-dsl-backend-module.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inversify_1 = require("inversify");
-var node_1 = require("@theia/languages/lib/node");
-var mdr_dsl_language_server_contribution_1 = require("./mdr-dsl-language-server-contribution");
-exports.default = new inversify_1.ContainerModule(function (bind) {
-    bind(node_1.LanguageServerContribution).to(mdr_dsl_language_server_contribution_1.MdrLanguageServerContribution).inSingletonScope();
-});
-//# sourceMappingURL=mdr-dsl-backend-module.js.map
