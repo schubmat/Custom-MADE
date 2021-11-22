24a851a865202b4607503984df35eaec23eec689
Mathias Schubanz <M.Schubanz@b-tu.de>
2021-11-22 09:35:19 +0100

    Deleted unnecesary code

--- backend/src/main/java/de/btu/swt/backend/lsp/LspController.java
+++ backend/src/main/java/de/btu/swt/backend/lsp/LspController.java
@@ -30,27 +30,16 @@ import java.util.Optional;
 public class LspController {
 
     private LspRepository lspRepository;
-//    private LspInstanceRepository instanceRepository;
-    private UserRepository userRepository;
 
     @Autowired
-    public LspController(LspRepository lspRepository, UserRepository userRepository) {
+    public LspController(LspRepository lspRepository) {
         this.lspRepository = lspRepository;
-//        this.instanceRepository = instanceRepository;
-        this.userRepository = userRepository;
     }
 
     @Bean
     CommandLineRunner afterDataInitialize(LspRepository lspRepository) {
-        return null;/*
-        buildLspBinaries();
-        return args -> {
-            log.info("afterDataInitialize");
-            List<LanguageServer> lsps = lspRepository.findAll();
-            for (LanguageServer lsp : lsps) {
-                startLspInstance(lsp);
-            }
-        };*/
+        return null;
+//        buildLspBinaries();
     }
 
     private void buildLspBinaries() {
@@ -60,72 +49,18 @@ public class LspController {
         log.info(ScriptRunner.run(script, CommandSupplements.INIT.toString()));
     }
 
-//    private LspInstance startLspInstance(LanguageServer languageServer) {
-//        int freePort = Port.getPort();
-//        LspInstance lspInstance = LspInstance.builder()
-//                .languageServer(languageServer)
-//                .port(freePort)
-//                .build();
-//        lspInstance = instanceRepository.save(lspInstance);
-//        log.info("Starting lsp instance on port: " + freePort);
-//        lspInstance.start();
-//        return null;
-//    }
-
     @GetMapping("/{lspId}")
     public ResponseEntity getLspInstance(@AuthenticationPrincipal UserDetails userDetails, @PathVariable long lspId) {
 
-//        Optional<User> opt = userRepository.findByUsername(userDetails.getUsername());
-//        if (!opt.isPresent())
-//            return null;
-//        User user = opt.get();
-//
-//        if (!validate(user, lspId, Permissions.USE))
-//            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("No binaries for the language server exists or user does not have permissions to browse file");
-//        LanguageServer lsp = lspRepository.getOne(lspId);
-//        LspInstance lspInstance;
-//        List<LspInstance> freeInstances = lsp.getFreeInstances();
-//        if (freeInstances.isEmpty()) {
-//            log.warn("LSP "+lsp.getLanguageName()+" has no free instance");
-//            lspInstance = startLspInstance(lsp);
-//        }
-//        else {
-//            lspInstance = freeInstances.get(0);
-//        }
-//        if (freeInstances.size() <= 1) {
-//            //start new instance for the next request
-////            startLspInstance(lsp);
-//        }
-//        lspInstance.setUser(user);
-//        instanceRepository.save(lspInstance);
-//        lspInstance.blockUntilReady();
         return null; // ResponseEntity.ok(LspInstanceDTOUtils.lspInstanceToDTO(lspInstance));
     }
 
     @PostMapping(value = "/instances/{instanceId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity removeLspInstance(@AuthenticationPrincipal User userDetails, @PathVariable long instanceId) {
-//        Optional<LspInstance> optionalInstance = instanceRepository.findById(instanceId);
-//        if (!optionalInstance.isPresent()) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("There is no lsp-instance with given id: " + instanceId);
-//        }
-//        LspInstance lspInstance = optionalInstance.get();
-//        if (lspInstance.getUser().getId().equals(userDetails.getId())) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User "+ userDetails.getUsername() + " is not allowed to kill lsp-instance " + instanceId);
-//        }
-//        log.info("Stopping lsp instance on port: " + lspInstance.getPort());
-//        lspInstance.stop();
-//        instanceRepository.delete(lspInstance);
-        return ResponseEntity.ok("Stopped lsp instance.");
+
+    	return ResponseEntity.ok("Stopped lsp instance.");
     }
-
-//    @PreDestroy
-//    public void destroy() {
-//        final ClassLoader classLoader = getClass().getClassLoader();
-////        final File script = new File(Objects.requireNonNull(classLoader.getResource("lsp/manageLSP.sh")).getFile());
-//        log.info("Killing all lsp instances: ");
-////        ScriptRunner.run(script, CommandSupplements.KILL_ALL.toString());
-//    }
-
+    
     private boolean validate(User user, long id, Permissions actions) {
         LanguageServer lsp = lspRepository.getOne(id);
         return lsp.getVersion().getPermissions(user).contains(actions);
