9bb1aa55e3aa04f6a516bd0d53fe110bb1479b2e
Mathias Schubanz <M.Schubanz@b-tu.de>
2022-11-05 20:16:26 +0100

    Revised minor typo

--- runDockerContainer.sh
+++ runDockerContainer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo docker build -t custom-Made .
-sudo docker run -p 3000:3000 -p 3001:3001 -it custom-Made
+sudo docker build -t custom-made .
+sudo docker run -p 3000:3000 -p 3001:3001 -it custom-made
 
 #### once the tty has started, type in:
 #
